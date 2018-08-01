efbc3c73882d8eac56f51e205d3b3c718f9f766d
Peteromuya <pomuya89@example.com>
2018-08-01 22:38:03 +0300

    Fix models

--- models.py
+++ models.py
@@ -7,16 +7,48 @@ import datetime
 from flask import make_response, jsonify, current_app
 from werkzeug.security import generate_password_hash
 
-
-
 import config
 
-  
 import psycopg2
-import databasesetup
-    
-class User(object):
+
+conn=psycopg2.connect("dbname='challenge3' user='postgres' password='0725401106' host='localhost' port='5435'")
+cur = conn.cursor()
+
+all_users = {}
+user_count = 1
+
+all_entries = {}
+entry_count = 1
+
+class MyDatabase:
+    """My Diary Database methods"""
+    def __init__(self):
+        conn=psycopg2.connect("dbname='challenge3' user='postgres' password='0725401106' host='localhost' port='5435'")
+        cur=conn.cursor()
+
+
+    def create_table():
+        cur.execute("CREATE TABLE IF NOT EXISTS users (username VARCHAR, password VARCHAR, email TEXT)")
+        cur.execute("CREATE TABLE IF NOT EXISTS entries (user_id INTEGER, entry TEXT, date TEXT)")
+        conn.commit()
+        conn.close()
+
+    def insert(username,email,password):
+        db_cursor.execute("INSERT INTO users VALUES('%s','%s','%s')", (username,email,password))
+        db_cursor.execute("INSERT INTO entries VALUES('%s','%s','%s')", (user_id,entry,date))
+        db_connection.commit()
+        db_connection.close()
+
+
+
+    def create_con(self):
+        conn=psycopg2.connect("dbname='challenge3' user='postgres' password='0725401106' host='localhost' port='5435'")
+        return conn
+
+
+class User():
     """Contains user columns and methods to add, update and delete a user"""
+
 
     def __init__(self, username, email, password, admin):
         self.username = username
@@ -33,12 +65,9 @@ class User(object):
         db_cursor.execute(new_user)
         db.commit()
 
-
     @staticmethod
     def create_user(username, email, password, admin=False, **kwargs):
         """Creates a new user and appends his information to the all_users dictionary"""
-       
-       
         global all_users
         global user_count
         all_users[user_count] = {"id": user_count, "username" : username,
@@ -47,17 +76,31 @@ class User(object):
         user_count += 1
         return new_user
 
-    @staticmethod              
+
+
+
+    @staticmethod
     def update_user(user_id, username, email, password, admin):
         """Updates user information"""
         try:
             db_cursor = db.con()
-            db_cursor.execute("UPDATE users SET username=%s, email=%s, password=%s WHERE user_id=%s",
-                                (username, email, password, user_id))
+            db_cursor.execute("UPDATE users SET username=%s, email=%s, password=%s, admin=%s WHERE user_id=%s",
+                                (username, email, password, admin, user_id))
             db.commit()
             return make_response(jsonify({"message" : "user has been successfully updated"}), 200)
         except:
-            return make_response(jsonify({"message" : "user does not exist"}), 400)
+            return make_response(jsonify({"message" : "user does not exist"}), 404)
+
+    @staticmethod
+    def delete_user(user_id):
+        """Deletes a user"""
+        try:
+            db_cursor = db.con()
+            db_cursor.execute("DELETE FROM users WHERE user_id=%s", (user_id,))
+            db.commit()
+            return make_response(jsonify({"message" : "user has been successfully deleted"}), 200)
+        except:
+            return make_response(jsonify({"message" : "user does not exists"}), 404)
 
     @staticmethod
     def get_user(user_id):
@@ -74,7 +117,6 @@ class User(object):
             return make_response(jsonify({"profile" : info}), 200)
         return make_response(jsonify({"message" : "user does not exists"}), 404)
 
-
     @staticmethod
     def get_all_users():
         """Gets all users"""
@@ -90,46 +132,48 @@ class User(object):
             all_users.append(info)
         return make_response(jsonify({"All users" : all_users}), 200)
 
+
 class Entry():
     """Contains entry columns and methods to add, update and delete an entry"""
 
 
-    def __init__(self, entry, user_id):
+    def __init__(self, entry, user_id, date):
         self.entry = entry
         self.user_id = user_id
-        
-        new_entry = "INSERT INTO entries (entry, user_id) VALUES " \
-                    "('" + self.entry + "', '" + self.user_id + "' )"
+        self.date = date
+
+
+        new_entry = "INSERT INTO entries (entry, user_id, date) VALUES " \
+                    "('" + self.entry + "', '" + self.user_id + "', '" + self.date + "' )"
         db_cursor = db.con()
         db_cursor.execute(new_entry)
         db.commit()
 
     @classmethod
-    def create_entry(cls, entry, user_id):
+    def create_entry(cls, entry, user_id, date):
         """Creates a new entry"""
 
-        cls(entry, user_id)
+        cls(entry, user_id, date)
         return make_response(jsonify({"message" : "entry has been successfully created"}), 201)
 
     @staticmethod
-    def update_entry(entry_id, entry, user_id):
+    def update_entry(entry_id, entry, user_id, date):
         """Updates entry information"""
         try:
             db_cursor = db.con()
-            db_cursor.execute("UPDATE entries SET entry=%s, user_id=%s WHERE entry_id=%s",
-                                  (entry, user_id, entry_id))
+            db_cursor.execute("UPDATE entries SET entry=%s, user_id=%s, date=%s WHERE entry_id=%s",
+                                  (entry, user_id, date,entry_id))
             db.commit()
-            return make_response(jsonify({"message" : "entry has been successfully updated"}), 200)
+            return make_response(jsonify({"message" : "user has been successfully updated"}), 200)
         except:
-            return make_response(jsonify({"message" : "entry does not exist"}), 404)
-
+            return make_response(jsonify({"message" : "user does not exist"}), 404)
 
     @staticmethod
     def delete_entry(entry_id):
         """Deletes an entry"""
         db_cursor = db.con()
         db_cursor.execute("SELECT * FROM entries")
-        entries = db_cursor.fetchall()
+        rides = db_cursor.fetchall()
 
         for entry in entries:
             if entry[0] == entry_id:
@@ -137,32 +181,33 @@ class Entry():
                 db.commit()
 
                 return make_response(jsonify({"message" : "entry has been successfully deleted"}), 200)
-        return make_response(jsonify({"message" : "entry does not exists"}), 404)
+        return make_response(jsonify({"message" : "user does not exists"}), 404)
 
     @staticmethod
     def get_entry(entry_id):
         """Gets a particular entry"""
         db_cursor = db.con()
         db_cursor.execute("SELECT * FROM entries WHERE entry_id=%s", (entry_id,))
-        ride = db_cursor.fetchall()
+        entry = db_cursor.fetchall()
 
         if entry != []:
             entry=entry[0]
             info = {entry[0] : {"entry": entry[1],
-                                "user_id": entry[2]}}
+                                "user_id": entry[2],
+                                "date": entry[3]}}
             return make_response(jsonify({"entry" : info}), 200)
         return make_response(jsonify({"message" : "entry does not exists"}), 404)
-        
+
     @staticmethod
     def get_all_entries():
         """Gets all entries"""
         db_cursor = db.con()
         db_cursor.execute("SELECT * FROM entries")
-        rides = db_cursor.fetchall()
+        entries = db_cursor.fetchall()
         all_entries = []
         for entry in entries:
             info = {entry[0] : {"entry": entry[1],
-                                "user_id": entry[2]}}
+                                "user_id": entry[2],
+                                "date": entry[3]}}
             all_entries.append(info)
         return make_response(jsonify({"All entries" : all_entries}), 200)
-
