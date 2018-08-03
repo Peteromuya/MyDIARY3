93e2074f585ab02752669a29010e0fe3f970a14e
Peteromuya <pomuya89@example.com>
2018-08-03 09:25:59 +0300

    Fix travis

---
+++ .travis.sh
@@ -0,0 +1,4 @@
+
+export SECRET_KEY="!@#_)&^%$$epic8^%%$#@#%^&*(&^&"
+export test_db=dbname='travis_tests' user='postgres' password='' host='localhost'
+export production_db=dbname='travis_tests' user='postgres' password='' host='localhost'
